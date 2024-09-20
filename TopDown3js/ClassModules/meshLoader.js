77e338e139157cbced378ba364996bb10b10c5a1
Prydwen-kun <118287298+Prydwen-kun@users.noreply.github.com>
2024-09-20 15:03:35 +0200

    Create meshLoader.js

---
+++ TopDown3js/ClassModules/meshLoader.js
@@ -0,0 +1,12 @@
+import * as THREE from "../three.js-master/build/three.module.js";
+import * as CANNON from "cannon-es";
+import * as CANNON_INIT from "./cannon_init.js";
+import { GLTFLoader } from "../three.js-master/examples/jsm/loaders/GLTFLoader.js";
+
+class meshLoader {
+  constructor() {
+   
+  }
+}
+
+export { meshLoader };
