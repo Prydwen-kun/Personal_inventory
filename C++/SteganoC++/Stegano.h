17f510599f3c8a4490ca2c47b4ba573871967cd1
Prydwen-kun <118287298+Prydwen-kun@users.noreply.github.com>
2024-09-27 12:19:07 +0200

    steganoC++

---
+++ C++/SteganoC++/Stegano.h
@@ -0,0 +1,23 @@
+﻿// SteganoC++.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+// TODO: Reference additional headers your program requires here.
+class Stegano {
+private:
+	ifstream inputImage;
+	ofstream outputImage;
+
+public:
+
+	Stegano(string filePath);
+	int encryptMessage(string message);
+	int decryptMessage(string filePath);
+};
---
+++ python/main.py
@@ -0,0 +1,4 @@
+userInput = '0'
+while userInput != 'no':
+    userInput = input('Continue ? yes/no :')
+    print(userInput)
