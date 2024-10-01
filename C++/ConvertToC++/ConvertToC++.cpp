5bf3ee25b66f46f5974237a112411865cfabbbd5
Prydwen-kun <118287298+Prydwen-kun@users.noreply.github.com>
2024-10-01 16:45:44 +0200

    binary

--- C++/ConvertToC++/ConvertToC++.cpp
+++ C++/ConvertToC++/ConvertToC++.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <bitset>
 
 #define readbin std::ios::binary | std::ios::in
 #define writebin std::ios::binary | std::ios::out
@@ -17,23 +18,45 @@ using namespace std;
 int main()
 {
 	std::cout << "Input File :\n";
-	ifstream fileInput("Input.txt", readbin);
+	ifstream fileInput("InputImage.bmp", readbin);
 
 	if (!fileInput) {
-		std::cerr << "Error opening file!" << std::endl;
+		cerr << "Error opening file!" << endl;
 		cout << "creating Input.txt instead " << endl;
 		system("type nul >> Input.txt");
 		ifstream fileInput("Input.txt", readbin);
 	}
-	
-	stringstream buffer;
-	buffer << fileInput.rdbuf();
-	string fileDump = buffer.str();
-	cout << fileDump << endl;
 
+	//READ HEADER DATA	
+
+	filebuf* fileBuffer = fileInput.rdbuf();
+	size_t size = fileBuffer->pubseekoff(0, fileInput.end, fileInput.in);
+	fileBuffer->pubseekpos(0, fileInput.in);
+
+	char* buffer = new char[size];
+	fileBuffer->sgetn(buffer, size);
+
+	//cout.write(buffer, size);
+	string* bit = new string[size];
+	char* result = new char[size];
+
+	for (int i = 0; i < size; ++i) {
+
+		bit[i] = bitset<8>(buffer[i]).to_string();
+		cout << "|";
+		cout.write(bit[i].c_str(), size);
+		cout << "|";
+		if (i % 11 == 0) {
+			cout << endl;
+		}
+	}
 
 
 	fileInput.close();
+	delete[] bit;
+	delete[] result;
+	delete[] buffer;
+
 	return 0;
 }
 
